d9b8580208819f5d4fb473af66bf93f86f7e6e0d
Mouhsen Ibrahim <mouhsen.ibrahim@gmail.com>
2023-03-21 15:36:09 +0100

    initial commit

---
+++ kuberpult.tf
@@ -0,0 +1,27 @@
+resource "helm_release" "kuberpult" {
+  name  = "kuberpult"
+  chart = "https://github.com/freiheit-com/kuberpult/releases/download/0.4.44/kuberpult-0.4.44.tgz"
+
+  cleanup_on_fail = true
+
+  set {
+    name  = "git.url"
+    value = "https://github.com/mouhsen-ibrahim/test-manifests.git"
+  }
+
+  set {
+    name  = "git.branch"
+    value = "main"
+  }
+
+  set {
+    name  = "ingress.domainName"
+    value = "kuberpult.test.com"
+  }
+
+  set {
+    name  = "ingress.iap.enabled"
+    value = "false"
+  }
+
+}
