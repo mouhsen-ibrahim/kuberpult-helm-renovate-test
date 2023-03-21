087f0cb7cf5b4434150bb08d18fde49c6efd90c4
Mouhsen Ibrahim <mouhsen.ibrahim@gmail.com>
2023-03-21 15:54:43 +0100

    Add cert-manager

---
+++ cert-manager.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.10.0"
+
+  cleanup_on_fail = true
+
+}
